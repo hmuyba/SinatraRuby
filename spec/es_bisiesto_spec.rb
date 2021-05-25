18e451c6c2fef02ee3a6d268a266aed08b42d3a9
Harold Muyba <hmuyba@gmail.com>
2021-05-25 08:26:14 -0400

    segundo ciclo de tdd

--- spec/es_bisiesto_spec.rb
+++ spec/es_bisiesto_spec.rb
@@ -1,8 +1,12 @@
 require './lib/bisiesto.rb'
 
 RSpec.describe "bisesto" do
-    it "deberia devolver true para el año 4" do
+    it "deberia devolver true para el anio 4" do
         expect(es_bisiesto(4)).to eq(true)
+    end
+
+    it "deberia devolver false para el anio 5"do
+    expect(es_bisiesto(5)).to eq(false)
     end
 end
 
